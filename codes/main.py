3fb5de607ade2478713ef018dd70524c3603ec95
lincyaw <814750204@qq.com>
2025-06-19 20:58:49 +0800

    format code

--- codes/main.py
+++ codes/main.py
@@ -1,7 +1,9 @@
 from torch.utils.data import Dataset, DataLoader
 import torch
 import dgl
-class chunkDataset(Dataset): #[node_num, T, else]
+
+
+class chunkDataset(Dataset):  # [node_num, T, else]
     def __init__(self, chunks, node_num, edges):
         self.data = []
         self.idx2id = {}
@@ -13,18 +15,22 @@ class chunkDataset(Dataset): #[node_num, T, else]
             graph.ndata["metrics"] = torch.FloatTensor(chunk["metrics"])
             graph.ndata["traces"] = torch.FloatTensor(chunk["traces"])
             self.data.append((graph, chunk["culprit"]))
-                
+
     def __len__(self):
         return len(self.data)
+
     def __getitem__(self, idx):
         return self.data[idx]
+
     def __get_chunk_id__(self, idx):
         return self.idx2id[idx]
+
 
 from utils import *
 from base import BaseModel
 
 import argparse
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--random_seed", default=42, type=int)
 
@@ -39,18 +45,20 @@ parser.add_argument("--patience", default=10, type=int)
 parser.add_argument("--self_attn", default=True, type=lambda x: x.lower() == "true")
 parser.add_argument("--fuse_dim", default=128, type=int)
 parser.add_argument("--alpha", default=0.5, type=float)
-parser.add_argument("--locate_hiddens", default=[64], type=int, nargs='+')
-parser.add_argument("--detect_hiddens", default=[64], type=int, nargs='+')
+parser.add_argument("--locate_hiddens", default=[64], type=int, nargs="+")
+parser.add_argument("--detect_hiddens", default=[64], type=int, nargs="+")
 
 ##### Source params
 parser.add_argument("--log_dim", default=16, type=int)
-parser.add_argument("--trace_kernel_sizes", default=[2], type=int, nargs='+')
-parser.add_argument("--trace_hiddens", default=[64], type=int, nargs='+')
-parser.add_argument("--metric_kernel_sizes", default=[2], type=int, nargs='+')
-parser.add_argument("--metric_hiddens", default=[64], type=int, nargs='+')
-parser.add_argument("--graph_hiddens", default=[64], type=int, nargs='+')
+parser.add_argument("--trace_kernel_sizes", default=[2], type=int, nargs="+")
+parser.add_argument("--trace_hiddens", default=[64], type=int, nargs="+")
+parser.add_argument("--metric_kernel_sizes", default=[2], type=int, nargs="+")
+parser.add_argument("--metric_hiddens", default=[64], type=int, nargs="+")
+parser.add_argument("--graph_hiddens", default=[64], type=int, nargs="+")
 parser.add_argument("--attn_head", default=4, type=int, help="For gat or gat-v2")
-parser.add_argument("--activation", default=0.2, type=float, help="use LeakyReLU, shoule be in (0,1)")
+parser.add_argument(
+    "--activation", default=0.2, type=float, help="use LeakyReLU, shoule be in (0,1)"
+)
 
 ##### Data params
 parser.add_argument("--data", type=str, required=True)
@@ -59,24 +67,31 @@ parser.add_argument("--result_dir", default="../result/")
 params = vars(parser.parse_args())
 
 import logging
+
+
 def get_device(gpu):
     if gpu and torch.cuda.is_available():
         logging.info("Using GPU...")
         return torch.device("cuda")
     logging.info("Using CPU...")
     return torch.device("cpu")
-    
+
 
 def collate(data):
     graphs, labels = map(list, zip(*data))
     batched_graph = dgl.batch(graphs)
-    return batched_graph , torch.tensor(labels)
+    return batched_graph, torch.tensor(labels)
+
 
 def run(evaluation_epoch=10):
     data_dir = os.path.join("../chunks", params["data"])
 
     metadata = read_json(os.path.join(data_dir, "metadata.json"))
-    event_num, node_num, metric_num =  metadata["event_num"], metadata["node_num"], metadata["metric_num"]
+    event_num, node_num, metric_num = (
+        metadata["event_num"],
+        metadata["node_num"],
+        metadata["metric_num"],
+    )
     params["chunk_lenth"] = metadata["chunk_lenth"]
 
     hash_id = dump_params(params)
@@ -88,15 +103,28 @@ def run(evaluation_epoch=10):
 
     train_data = chunkDataset(train_chunks, node_num)
     test_data = chunkDataset(test_chunks, node_num)
-    
-    train_dl = DataLoader(train_data, batch_size=params["batch_size"], shuffle=True, collate_fn=collate, pin_memory=True)
-    test_dl = DataLoader(test_data, batch_size=params["batch_size"], shuffle=False, collate_fn=collate, pin_memory=True)
+
+    train_dl = DataLoader(
+        train_data,
+        batch_size=params["batch_size"],
+        shuffle=True,
+        collate_fn=collate,
+        pin_memory=True,
+    )
+    test_dl = DataLoader(
+        test_data,
+        batch_size=params["batch_size"],
+        shuffle=False,
+        collate_fn=collate,
+        pin_memory=True,
+    )
 
     model = BaseModel(event_num, metric_num, node_num, device, **params)
     scores, converge = model.fit(train_dl, test_dl, evaluation_epoch=evaluation_epoch)
 
     dump_scores(params["result_dir"], hash_id, scores, converge)
     logging.info("Current hash_id {}".format(hash_id))
+
 
 if "__main__" == __name__:
     run()
--- codes/utils.py
+++ codes/utils.py
@@ -1,6 +1,8 @@
 import os
 import logging
 import pickle
+import json
+
 
 def load_chunks(data_dir):
     logging.info("Load from {}".format(data_dir))
@@ -10,32 +12,56 @@ def load_chunks(data_dir):
         chunk_test = pickle.load(fr)
     return chunk_train, chunk_test
 
-import json
+
 def read_json(filepath):
     if os.path.exists(filepath):
-        assert filepath.endswith('.json')
-        with open(filepath, 'r') as f:
+        assert filepath.endswith(".json")
+        with open(filepath, "r") as f:
             return json.loads(f.read())
-    else: 
-        logging.raiseExceptions("File path "+filepath+" not exists!")
+    else:
+        logging.raiseExceptions("File path " + filepath + " not exists!")
         return
+
 
 def json_pretty_dump(obj, filename):
     with open(filename, "w") as fw:
-        json.dump(obj,fw, sort_keys=True, indent=4, separators=(",", ": "), ensure_ascii=False)
+        json.dump(
+            obj,
+            fw,
+            sort_keys=True,
+            indent=4,
+            separators=(",", ": "),
+            ensure_ascii=False,
+        )
 
 
 from datetime import datetime, timedelta
+
+
 def dump_scores(result_dir, hash_id, scores, converge):
-    with open(os.path.join(result_dir, 'experiments.txt'), 'a+') as fw:
-        fw.write(hash_id+': '+(datetime.now()+timedelta(hours=8)).strftime("%Y/%m/%d-%H:%M:%S")+'\n')
-        fw.write("* Test result -- " + '\t'.join(["{}:{:.4f}".format(k, v) for k,v in scores.items()])+'\n')
-        fw.write('Best score got at epoch: '+str(converge)+'\n')
-        fw.write('{}{}'.format('='*40, '\n'))
+    with open(os.path.join(result_dir, "experiments.txt"), "a+") as fw:
+        fw.write(
+            hash_id
+            + ": "
+            + (datetime.now() + timedelta(hours=8)).strftime("%Y/%m/%d-%H:%M:%S")
+            + "\n"
+        )
+        fw.write(
+            "* Test result -- "
+            + "\t".join(["{}:{:.4f}".format(k, v) for k, v in scores.items()])
+            + "\n"
+        )
+        fw.write("Best score got at epoch: " + str(converge) + "\n")
+        fw.write("{}{}".format("=" * 40, "\n"))
+
 
 import hashlib
+
+
 def dump_params(params):
-    hash_id = hashlib.md5(str(sorted([(k, v) for k, v in params.items()])).encode("utf-8")).hexdigest()[0:8]
+    hash_id = hashlib.md5(
+        str(sorted([(k, v) for k, v in params.items()])).encode("utf-8")
+    ).hexdigest()[0:8]
     result_dir = os.path.join(params["result_dir"], hash_id)
     os.makedirs(result_dir, exist_ok=True)
 
@@ -52,9 +78,12 @@ def dump_params(params):
     )
     return hash_id
 
-import random    
+
+import random
 import numpy as np
 import torch
+
+
 def seed_everything(seed=42):
     random.seed(seed)
     os.environ["PYTHONHASHSEED"] = str(seed)
