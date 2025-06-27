c8f55d74b9d0b14bb0c992740e7fc5716c83993a
lincyaw <814750204@qq.com>
2025-06-27 14:17:06 +0800

    add config

--- create_dataset.py
+++ create_dataset.py
@@ -2,5 +2,5 @@ from src.preprocessing.processor import Processor
 
 
 if __name__ == "__main__":
-    processor = Processor({})
+    processor = Processor({}, conf="settings.yaml")
     processor.process_dataset()
